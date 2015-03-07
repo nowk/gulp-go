a9d82dcb8b011b4066820717365891499bc275fc
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-03-06 23:38:34 -0500

    Pass callback

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ var fshut = function(callback) {
     var pid = go.proc.pid;
 
     go.stop(function() {
-      delps(pid); // delete from ps collection
-      fshut();    // shutdown the next process
+      delps(pid);      // delete from ps collection
+      fshut(callback); // shutdown the next process
     });
   }
 };
